e47a201d447e3491452c9cd51b3fb3d3180fd542
Jesus Reynaldo <jezuz.of.murder@gmail.com>
2022-05-28 01:58:29 -0400

    28/05/2022

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
+import {FaFacebookSquare, FaGithubSquare} from 'react-icons/fa'
 const HeaderSocials = () => {
     return (
         <div className="header_socials">
             <a href="https://linkedin.com" target="_blank"><BsLinkedin /></a>
-            <a href="https://github.com" target="_blank"></a>
-            <a href="https://facebook.com" target="_blank"></a>
+            <a href="https://github.com" target="_blank"><FaGithubSquare /></a>
+            <a href="https://facebook.com" target="_blank"><FaFacebookSquare /></a>
             
         </div>
     )
